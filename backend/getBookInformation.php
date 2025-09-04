32beb75d7fa1b590d80f9545b4bd109b5dc81a5b
Yusuf Bey <yusuf@Yusuf-MacBook-Air.local>
2025-09-04 14:06:48 +0300

    first commit

--- backend/getBookInformation.php
+++ backend/getBookInformation.php
@@ -2,7 +2,6 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Request-Headers: content-type,x-requested-with");
 header("Content-Type: application/json");
-// $getBookInformation = file_get_contents('https://www.kitapyurdu.com/kitap/sema-terapi/140891.html');
 
 require "../vendor/autoload.php";
 
@@ -19,7 +18,6 @@ $crawler->filter(".pr_header__heading")->each(function ($node) {
     global $bookName;
 
     $bookName = $node->filter('h1')->text();
-    // print_r($bookName . "<br>");     // kitap ismi
 });
 
 $bookCategory = array();
@@ -36,7 +34,6 @@ $crawler->filter(".book-front")->each(function ($node) {
     global $imgSource;
 
     $imgSource = $node->filter('a')->attr('href');
-    // print_r($imgSource . "<br>");     // kitap ismi
 });
 
 $writerAndPublishingHouse = array();
@@ -45,8 +42,6 @@ $crawler->filter(".pr_producers__item")->each(function ($node) {
     global $writerAndPublishingHouse;
 
     array_push($writerAndPublishingHouse, $node->filter('a')->text());
-
-    // print_r($writerAndPublishingHouse . PHP_EOL . "<br>"); // Yazarlar ve Yayın evi 
 });
 
 $bookPrice = "";
@@ -63,7 +58,6 @@ $crawler->filter("#description_text")->each(function ($node) {
     global $bookDescription;
 
     $bookDescription = $node->filter('span')->text();
-    // print_r($bookDescription . "<br>"); // Kitap Açıklaması
 });
 
 $detailsTitle = array();
@@ -71,7 +65,6 @@ $detailsContent = array();
 
 $crawler->filter("tr")->each(function ($node) {
     $tds = $node->filter('td');
-    // Kitap Detayları
 
     global $detailsTitle, $detailsContent;
 
